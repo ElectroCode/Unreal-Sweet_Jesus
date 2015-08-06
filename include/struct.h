167fa3cc28cb9c156f7231434a9f2b716d0dac98
Ken Spencer <iota@electrocode.net>
2015-08-05 23:25:00 -0400

    Change the TOPICLEN to 402... and there we have it, Unreal-Sweet_Jesus updated to 3.2.10.4

--- include/struct.h
+++ include/struct.h
@@ -175,7 +175,7 @@ typedef unsigned int u_int32_t;	/* XXX Hope this works! */
 #define	USERLEN		10
 #define	REALLEN	 	50
 #define SVIDLEN		30
-#define	TOPICLEN	307
+#define	TOPICLEN	402
 #define	CHANNELLEN	32
 #define	PASSWDLEN 	48	/* was 20, then 32, now 48. */
 #define	KEYLEN		23
